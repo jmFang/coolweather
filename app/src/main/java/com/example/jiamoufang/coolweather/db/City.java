eb43ced624403269e9e9fb38b580e95682a35b83
jmFang <fangjiamou.com@outlook.com>
2017-09-16 10:43:55 +0800

    加入数据库和表的各项配置

---
+++ app/src/main/java/com/example/jiamoufang/coolweather/db/City.java
@@ -0,0 +1,45 @@
+package com.example.jiamoufang.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by jiamoufang on 2017/9/16.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
